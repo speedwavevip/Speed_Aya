2f314c9bb4f22cd34246975171e26912f9c93a85
SPEED.EXE <speedwave904@gmail.com>
2025-03-21 14:18:24 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,6 +1,4 @@
 local Games = {
   [2753915549] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blox%20Sea1.lua", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blox%20Sea1.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blox%20Sea1.lua", -- Blox Fruit Sea 3
 }
 return Games
