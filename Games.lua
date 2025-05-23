6c92ba517bbcc8ba0b99191cb67e306ebe47dc6d
SPEED.EXE <speedwave904@gmail.com>
2025-05-24 00:55:38 +1300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -2,8 +2,8 @@ local Games = {
   [2753915549] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blox%20Sea1.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blox%20Sea2.lua", -- Blox Fruit Sea 2
   [4924922222] = "https://raw.githubusercontent.com/speedwavevip/Main/refs/heads/main/KeyBrookaven", -- Brookhaven
-  [116495829188952] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Dead%20Ralis", -- Dead Ralis
-  [70876832253163] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Dead%20Ralis", -- Dead Ralis
+  [116495829188952] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Dead%20Ralis%20Arc.lua", -- Dead Ralis
+  [70876832253163] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Dead%20Ralis%20Arc.lua", -- Dead Ralis
   [85896571713843] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Key%20Bubble%20Gum%20Simulator", -- Bubble Gum Simulator
 }
 return Games
