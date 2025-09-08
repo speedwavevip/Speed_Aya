0bfad8b1714afc7dc2e290c018e234bfb65c1286
SPEED.EXE <speedwave904@gmail.com>
2025-09-08 19:52:41 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,3 +1,4 @@
 local Games = {
-  [13772394625] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blade%20Ball.lua" -- Blade Ball
+  [13772394625] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blade%20Ball.lua", -- Blade Ball
 }
+return Games
