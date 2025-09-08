04898c0d0bb67084594c0abe463a0ffef5603213
SPEED.EXE <speedwave904@gmail.com>
2025-09-08 19:48:27 +0300

    Create SPEED WAVE.lua

---
+++ SPEED WAVE.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Games.lua"))()
+
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
