500a6d8ac6b6b8f107d51d348618f976055189c0
SPEED.EXE <speedwave904@gmail.com>
2025-09-26 08:25:28 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,4 +1,5 @@
 local Games = {
   [13772394625] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blade%20Ball.lua", -- Blade Ball
+  [5523851880] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/8ballpool.lua", -- 🐰 8-Ball Pool Classic 🎱
 }
 return Games
