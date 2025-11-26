2173eaa991b1a67de0a27a701f0f82a959289bbc
SPEED.EXE <speedwave904@gmail.com>
2025-11-26 16:22:14 +0300

    Delete Games.lua

--- Games.lua
+++
@@ -1,8 +0,0 @@
-local Games = {
-  [13772394625] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blade%20Ball.lua", -- Blade Ball
-  [5523851880] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/8ballpool.lua", -- 🐰 8-Ball Pool Classic 🎱
-  [127742093697776] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Plant%20vs%20Brainrot.lua", -- Plant vs Brainrot
-  [142823291] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/FarmMM2.lua", -- MM2
-  [121864768012064] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Fish%20it.lua", -- fish it
-}
-return Games
