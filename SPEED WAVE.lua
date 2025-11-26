e349275f4d8d269be1810dc880fc6e7e4289f8e3
SPEED.EXE <speedwave904@gmail.com>
2025-11-26 16:22:35 +0300

    Delete SPEED WAVE.lua

--- SPEED WAVE.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Games.lua"))()
-
-local URL = Games[game.PlaceId]
-
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
